7d14baffbb8a68f77dd3becc6d35221009abcd3c
juliannzaba <126623340+juliannzaba@users.noreply.github.com>
2023-03-08 11:59:59 -0300

    Add files via upload

---
+++ cvjulian.js
@@ -0,0 +1,16 @@
+
+document.getElementById("boton").onclick = function () { 
+    console.log("3564573659");
+    document.getElementById("cel").innerHTML = "3564573659"
+}  
+
+document.getElementById("botdir").onclick = function () { 
+    console.log("Solis 2775, San Francisco, Cordoba");
+    document.getElementById("Direccion").innerHTML = "Solis 2775, San Francisco, Cordoba"
+}
+
+
+document.getElementById("botmail").onclick = function () { 
+    console.log("juliannzaba@gmail.com");
+    document.getElementById("Email").innerHTML = "juliannzaba@gmail.com"
+}
